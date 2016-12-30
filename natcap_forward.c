f0726d6894ed0307bb739ff6e437a75a576690fe
Chen Minqiang <ptpt52@gmail.com>
2016-12-30 16:19:39 +0800

    fix print

--- natcap_forward.c
+++ natcap_forward.c
@@ -163,7 +163,7 @@ static unsigned int natcap_forward_pre_ct_in_hook(void *priv,
 
 bypass_out:
 		set_bit(IPS_NATCAP_BYPASS_BIT, &ct->status);
-		NATCAP_WARN("(FPCI)" DEBUG_UDP_FMT ": first packet in but not ctrl code\n", DEBUG_UDP_ARG(iph,l4));
+		NATCAP_DEBUG("(FPCI)" DEBUG_UDP_FMT ": first packet in but not ctrl code\n", DEBUG_UDP_ARG(iph,l4));
 	}
 
 	return NF_ACCEPT;
