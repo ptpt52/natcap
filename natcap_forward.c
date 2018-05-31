54ab56515c2e488b8d352c1104e149c867b721a7
Chen Minqiang <ptpt52@gmail.com>
2018-05-31 13:41:19 +0800

    update license notes

--- natcap_forward.c
+++ natcap_forward.c
@@ -1,6 +1,22 @@
 /*
  * Author: Chen Minqiang <ptpt52@gmail.com>
  *  Date : Sun, 05 Jun 2016 16:23:40 +0800
+ *
+ * This file is part of the natcap.
+ *
+ * natcap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * natcap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with natcap; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 #include <linux/module.h>
 #include <linux/version.h>
