5d475155f9289b3abff6894642c6d78d2f668065
Chen Minqiang <ptpt52@gmail.com>
2018-08-30 16:32:22 +0800

    init natcap_peer

---
+++ natcap_peer.h
@@ -0,0 +1,26 @@
+/*
+ * Author: Chen Minqiang <ptpt52@gmail.com>
+ *  Date : Thu, 30 Aug 2018 11:25:35 +0800
+ *
+ * This file is part of the natcap.
+ *
+ * natcap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * natcap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with natcap; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#ifndef _NATCAP_PEER_H_
+#define _NATCAP_PEER_H_
+
+
+
+#endif /* _NATCAP_PEER_H_ */
