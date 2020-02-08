6e9d41abba6b8541b903a6ed62c3bd5273eeb197
Chen Minqiang <ptpt52@gmail.com>
2020-02-08 19:39:04 +0800

    DNS ANS

--- natcap_client.h
+++ natcap_client.h
@@ -87,4 +87,41 @@ extern void natcap_rx_speed_set(int speed);
 extern int natcap_tx_speed_get(void);
 extern int natcap_rx_speed_get(void);
 
+/* for DNS decode */
+static inline int get_rdata(const unsigned char *src_ptr, int src_len, int src_pos, unsigned char *dst_ptr, int dst_size)
+{
+	int ptr_count = 0;
+	int ptr_limit = src_len / 2;
+	int pos = src_pos;
+	int dst_len = 0;
+	unsigned int v;
+	while (dst_len < dst_size && pos < src_len && (v = get_byte1(src_ptr + pos)) != 0) {
+		if (v > 0x3f) {
+			if (pos + 1 >= src_len) {
+				return -1;
+			}
+			if (++ptr_count >= ptr_limit) {
+				return -2;
+			}
+			pos = ntohs(get_byte2(src_ptr + pos)) & 0x3fff;
+			continue;
+		} else {
+			if (pos + v >= src_len) {
+				return -3;
+			}
+			if (dst_len + v >= dst_size) {
+				return -4;
+			}
+			memcpy(dst_ptr, src_ptr + pos + 1, v);
+			dst_ptr += v;
+			*dst_ptr = '.';
+			dst_ptr += 1;
+			dst_len += v + 1;
+			pos += v + 1;
+		}
+	}
+
+	return dst_len;
+}
+
 #endif /* _NATCAP_CLIENT_H_ */
