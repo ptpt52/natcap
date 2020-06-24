d006efd79230be89041d34c61a3bd3b00e193463
Chen Minqiang <ptpt52@gmail.com>
2020-06-24 16:24:35 +0800

    add res

---
+++ dubai.set.sh
@@ -0,0 +1,15 @@
+cat urls.js | \
+		grep -o "\(http\|https\)://.*" | \
+		cut -d"/" -f3 | \
+		grep -v '*' | \
+		sort | uniq | \
+		grep -v '\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)' \
+		>gfwlist1.tocn.host.txt
+
+cat urls.js | \
+		grep -o "\(http\|https\)://.*" | \
+		cut -d"/" -f3 | \
+		grep -v '*' | \
+		sort | uniq | \
+		grep -v '\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)' \
+		>gfwlist1.tocn.ip.txt
