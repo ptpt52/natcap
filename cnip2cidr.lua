1e7654771fa5fd7d8deb9ba0fa27cce87d1dea49
Chen Minqiang <ptpt52@gmail.com>
2021-10-25 18:22:08 +0800

    update iplists

--- cnip2cidr.lua
+++ cnip2cidr.lua
@@ -5,7 +5,7 @@ local rangeSet = {}
 for line in io.lines("ip.merge.txt") do
 	local ip1, ip2, z1, _, z2 = line:match('(%d+.%d+.%d+.%d+)|(%d+.%d+.%d+.%d+)|(%S-)|(%S-)|(%S-)|.*')
 	--print(z1, z2)
-	if z1 == '中国' and z2 ~= '香港' and z2 ~= '台湾' then
+	if z1 == '中国' and z2 ~= '香港' and z2 ~= '台湾省' and z2 ~= '澳门' then
 		local netString = string.format("%s-%s", ip1, ip2)
 		rangeSet = ipops.rangeSet_add_range(rangeSet, ipops.netString2range(netString))
 	end
